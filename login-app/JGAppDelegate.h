951fd8b06e5c7133024b406ded4ab74171308728
Michal Grman <mike.grman@gmail.com>
2014-05-14 19:28:51 +0200

    First commit

---
+++ login-app/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  login-app
+//
+//  Created by Mike on 14.5.2014.
+//  Copyright (c) 2014 J&G Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ login-app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  login-app
+//
+//  Created by Mike on 14.5.2014.
+//  Copyright (c) 2014 J&G Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JGAppDelegate class]));
+    }
+}
